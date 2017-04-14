7660a5012f40dca4cdd78714405de99c103de862
joeyhoek <joeyhoek07@hotmail.com>
2017-04-14 02:18:53 +0200

    Working

--- app/inApp.js
+++ app/inApp.js
@@ -5,7 +5,31 @@ window.onload = function () {
 		var window = remote.getCurrentWindow();
 		window.close();
 	});
+		
+	function getParams(parameters) {
+		//var window = remote.getCurrentWindow();
+		//if (parameters !== "" && parameters !== undefined && parameters.search(new RegExp(",")) !== false && parameters.search(new RegExp('//')) !== false && parameters.search(new RegExp("%")) !== false) {
+			parameters = parameters.replace("$", "&");
+			parameters = parameters.replace("$", "&");
+			parameters = parameters.replace("$", "&");
+			parameters = parameters.split(",");
+			parameters = parameters[1] + parameters[2];
+			parameters = parameters.replace("absence:\\\\", "");			
+			parameters = parameters.split("%");
+		//} else {
+		//	alert("Ongeldig verzoek");
+		//	window.close();
+		//}
+
+		return {
+			'qrCode': parameters[0],
+			'userId': parameters[1],
+			'token': parameters[2],
+			'classId': parameters[3]
+		};
+	}
 	
-	document.getElementById("hier").innerHTML = remote.process.argv;
+	var parameters = getParams(remote.process.argv + "");
+	document.getElementsByTagName("img")[0].src = parameters.qrCode;
 };
 
